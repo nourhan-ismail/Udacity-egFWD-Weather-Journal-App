2c00021e0bb14cdc4bf831d862576d6414b12181
Nourhan Ismail <73067280+nourhan-ismail@users.noreply.github.com>
2021-09-12 14:44:05 +0200

    Initialized express app and server listening to port 8888. Used cors and body-parser in middlewares

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
+const port = 8888;
+
 // Require Express to run server and routes
+const express = require("express");
+
+// Require Body-Parser to parse request object content
+const bodyParser = require("body-parser");
+
+// Require cors to solve cors issues
+const cors = require("cors");
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +21,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+app.use(cors());
 // Initialize the main project folder
-app.use(express.static('website'));
-
+app.use(express.static("website"));
 
 // Setup Server
+const server = app.listen(port, () => {
+  console.log("Listening to localhost:" + port);
+});
